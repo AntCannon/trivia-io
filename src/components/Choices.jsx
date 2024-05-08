3fcc8ea46a31d5d48ea2c2069f7170fee82886ca
AntCannon <anthonycannonier@gmail.com>
2024-05-08 17:24:01 -0400

    add randomize multiple choice

--- src/components/Choices.jsx
+++ src/components/Choices.jsx
@@ -5,14 +5,24 @@ export default function Choices({
   choices,
   handleSelect
 }) {
+  
+  if (choices.length === 4) {
+    const spread = [...choices]
+    const randomizedChoices = []
+    for (let i = 0; i < 4; i++) {
+      const randomN = Math.floor(Math.random()*spread.length)
+      randomizedChoices.push(spread.splice(randomN, 1)[0])
+    }
+    choices = randomizedChoices
+  }
 
   return (
     <ul className="choices">
       {choices.map((choice, i) => (
-        <Choice
-          choice={choice}
-          key={i}
-          handleSelect ={handleSelect}
+          <Choice
+            choice={choice}
+            key={i}
+            handleSelect ={handleSelect}
         />
       ))
       }
