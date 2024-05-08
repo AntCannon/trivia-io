695d2cb9671983c6d272565fa71ce7dae07c2a27
AntCannon <anthonycannonier@gmail.com>
2024-05-07 20:06:41 -0400

    add Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
+import Nav from './components/Nav.jsx'
 
 function App() {
 
   return (
     <>
+      <Nav />
     </>
   )
 }
